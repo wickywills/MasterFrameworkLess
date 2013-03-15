5b80a62cca447df4c48192f1ee3cbabe85592a82
wickywills <james@wickywills.com>
2013-03-15 17:32:11 +0000

    Added Flexslider

--- js/plugins.js
+++ js/plugins.js
@@ -4,13 +4,9 @@ $(document).ready(function() {
 	// Fancybox (http://fancyapps.com/fancybox/)
 	$(".fancybox").fancybox();
 
-	// Cycle (http://jquery.malsup.com/cycle/)
-	$('#cycle').cycle({
-        fx:     'fade',
-        speed:  '1000',
-        timeout: 3000,
-        pager:  '#nav',
-		slideExpr: 'img'
-    });
+	// Flexislider
+	$('.flexslider').flexslider({
+		animation: "slide"
+	});
 
 });
