1859d3897b78ba8ef879fcc8a012ed76e77a9a30
James Wills <james@wickywills.com>
2013-06-18 16:05:50 +0100

    Added "Uniform" form styling

--- js/plugins.js
+++ js/plugins.js
@@ -4,9 +4,14 @@ $(window).load(function() {
 	// Fancybox (http://fancyapps.com/fancybox/)
 	$(".fancybox").fancybox();
 
+
 	// Flexislider
 	$('.flexslider').flexslider({
 		animation: "slide"
 	});
+	
+
+	// Uniform form styling (http://uniformjs.com/)
+	$("select, input, a.button, button").uniform();
 
 });
